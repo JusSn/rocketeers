f3fdd3bc3e65ff34121264b32697797ae249bf7e
Alexander Wang <gnawang@umich.edu>
2017-02-27 01:41:49 -0500

    Set up base inputs; Player can throw in any direction

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -13,15 +13,20 @@ public class Item : MonoBehaviour {
 		rigid =	GetComponent<Rigidbody2D>();
 	}
 
-	public void PickedUp(Player _player){
+	public void Attach(Player _player){
+		rigid.velocity = Vector3.zero;
 		rigid.isKinematic = true;
 		transform.parent = _player.transform;
 		transform.localPosition = heldPos;
 	}
 
-	public void Thrown(Player _player, Vector3 throwVel){
+	public void Detach(Player _player){
 		rigid.isKinematic = false;
 		transform.parent = _player.transform.parent;
+	}
+
+	public void Thrown(Player _player, Vector3 throwVel){
+		Detach (_player);
 		rigid.velocity = throwVel;
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour {
 	public float 							xSpeed = 4f;
 	public float							ySpeed = 5f;
 	public float 							itemDetectRadius = 0.5f;
-	public float 							throwChargeMax = 10f;
+	public float 							throwChargeMax = 2.5f;
 	public float 							throwChargeRatio = 10f;
 	public bool 							debugMode = false;
 	public bool 							________________;
@@ -74,52 +74,60 @@ public class Player : MonoBehaviour {
 		stateUpdateMap [form] ();
 	}
 
-	/******************** State Modifiers & Updaters ********************/
+	/******************** State Modifiers & Behaviors ********************/
 
+	// General behavior of the player when not holding anything
+	// Entered from: Throwing(thrown), Setting(set)
+	// Exit to: Holding(pickup)
 	void NormalUpdate() {
 		CalculateMovement ();
 
 		// Check if an item is within reach
 		Collider2D itemCol;
 		if (itemCol = Physics2D.OverlapCircle (transform.position, itemDetectRadius, itemLayer)) {
-			if (Input.GetKey (KeyCode.A)) {
+			if (Input.GetButtonDown ("Pickup_P1")) {
 				Item held = itemCol.GetComponent<Item> ();
-				held.PickedUp (this);
+				held.Attach (this);
 				heldItem = held;
 				form = PlayerForm.Holding;
 			}
 		}
 	}
 
+	// Behavior when holding an item; Transitions to either throw or set
+	// Entered from: Normal(pickup), Throwing(cancel), Setting(cancel)
+	// Exit to: Throwing(throw button), Setting(set button)
 	void HoldingUpdate() {
 		CalculateMovement ();
 
 		// Switch to either throwing or setting
-		if (Input.GetKeyDown (KeyCode.S)) {
+		if (Input.GetAxis ("Throw_P1") > 0f) {
 			form = PlayerForm.Throwing;
-		} else if (Input.GetKeyDown (KeyCode.D)) {
+		} else if (Input.GetAxis ("Set_P1") > 0f) {
 			form = PlayerForm.Setting;
 		}
 	}
 
+	// Behavior when charging a throw; let go of the throw button to throw
+	// Entered from: Holding(throw button)
+	// Exit to: Holding(cancel), Normal(throw)
 	void ThrowingUpdate() {
 		CalculateMovement ();
 		throwChargeCount += Time.deltaTime;
 		sprend.color = Color.Lerp (Color.white, Color.red, throwChargeCount / throwChargeMax);
 
-		if (Input.GetKeyUp (KeyCode.S)) {
+		if (Input.GetAxis ("Throw_P1") == 0f) {
 			// Item is thrown
 			if (throwChargeCount > throwChargeMax) {
 				throwChargeCount = throwChargeMax;
 			}
-			// Replace base vector with controller input
-			Vector3 throwVel = new Vector3 (1f, 1f, 0f) * (throwChargeCount*throwChargeRatio);
+			Vector3 throwVel = GetAimDirection()*throwChargeCount*throwChargeRatio;
 			heldItem.Thrown (this, throwVel);
 			heldItem = null;
 			throwChargeCount = 0f;
 			sprend.color = Color.white;
 			form = PlayerForm.Normal;
-		} else if (Input.GetKeyUp (KeyCode.F)) {
+		} else if (Input.GetButtonDown ("Cancel_P1")) {
 			// Throwing was cancelled
 			throwChargeCount = 0f;
 			sprend.color = Color.white;
@@ -127,12 +135,21 @@ public class Player : MonoBehaviour {
 		}
 	}
 
+	// Behavior when preparing to set an item; let go of the set button to set
+	// Entered from: Holding(set button)
+	// Exit to: Holding(cancel), Normal(set)
 	void SettingUpdate() {
 		CalculateMovement ();
 
-		// TODO: WRITE SETTING BEHAVIOR
-		
-		if (Input.GetKeyUp (KeyCode.F)) {
+		Vector3 setPos = GetGridPosition ();
+		if (debugMode) {
+			Debug.DrawLine (transform.position, setPos, Color.red);
+		}
+
+		if (Input.GetAxis ("Set_P1") == 0f) {
+			
+			form = PlayerForm.Normal;
+		} else if (Input.GetButtonDown ("Cancel_P1")) {
 			// Setting was cancelled
 			form = PlayerForm.Holding;
 		}
@@ -182,20 +199,37 @@ public class Player : MonoBehaviour {
 
 	// Calculate and return magnitude of any changes to x velocity from player input
 	float GetXInputSpeed(float currentX) {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			currentX = -xSpeed;
-		} else if (Input.GetKey (KeyCode.RightArrow)) {
+		float direction = Input.GetAxis ("MoveX_P1");
+		if (0 < direction) {
 			currentX = xSpeed;
+		} else if (direction < 0) {
+			currentX = -xSpeed;
 		} 
 		return currentX;
 	}
 
 	// Calculate and return magnitude of any changes to y velocity from player input
 	float GetYInputSpeed(float currentY) {
-		if (Input.GetKey (KeyCode.Space) && grounded) {
+		if (Input.GetButtonDown ("Jump_P1") && grounded) {
 			currentY = ySpeed;
 		}
 		return currentY;
+	}
+
+	// Returns a normalized vector pointed toward the direction of the aiming joystick
+	Vector3 GetAimDirection() {
+		Vector3 inputDir = new Vector3 (Input.GetAxis ("AimX_P1"), Input.GetAxis ("AimY_P1"), 0f);
+		return inputDir.normalized;
+	}
+
+	// Return a vector3 of the location pointed to by the aiming joystick
+	// Rounded to nearest 0.5 (e.g. 1.2 rounds to 1.5, 0.8 rounds to 0.5, etc.)
+	Vector3 GetGridPosition() { 
+		Vector3 gridPos = transform.position + GetAimDirection ();
+		gridPos.x = Mathf.Floor (gridPos.x) + 0.5f;
+		gridPos.y = Mathf.Floor (gridPos.y) + 0.5f;
+		return gridPos;
+
 	}
 
 	// Return a bool checking if player object is standing on top of a block or ground
