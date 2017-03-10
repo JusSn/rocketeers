4188a81f42c02361f1aade51aef44db0d9e17f40
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-03-10 18:58:15 -0500

    Projectile tweaks; deets inside
    Having the trigger and regular box collider on the projectile was dumb.
    For some reason I thought I wanted to separate out collisions and triggers.
    Now it's all consolidated into collisions and will cause damage when the
    projectile collides with something.

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -5,30 +5,19 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     
     // inspector tunables
-    public float                                    bullet_speed = 4f;
-    public float                                    damage_amount = 25f;
-
+    public float                    bullet_speed = 4f;
+    public float                    damage_amount = 25f;
 
     // Use this for initialization
 	void Start () {
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    // calling condition: when the projectile hits another block
-    protected virtual void OnTriggerEnter2D(Collider2D other){
-
+    protected virtual void OnCollisionEnter2D(Collision2D other){
         // check if we came in contact with another block/weaponblock
         if (other.gameObject.CompareTag ("Block") || other.gameObject.CompareTag ("WeaponBlock")) {
             other.gameObject.GetComponent<Block> ().TakeDamage (damage_amount);
         }
-        Destroy(gameObject);
-    }
-
-    protected virtual void OnCollisionEnter2D(Collision2D other){
         Destroy(gameObject);
     }
 }
