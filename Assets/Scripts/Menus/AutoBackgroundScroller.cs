d5209cbcfdb5fc2dd4617e482f91c86b0b3e9865
Alexander Wang <gnawang@umich.edu>
2017-03-17 03:01:05 -0400

    Basic Main Menu

---
+++ Assets/Scripts/Menus/AutoBackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBackgroundScroller : MonoBehaviour {
+
+	public float			scrollSpeed = 1f;
+	public bool 			vertical = true;
+	private Renderer		rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		Vector2 offset;
+		if (vertical) {
+			offset = new Vector2 (0f, Time.time * scrollSpeed);
+		} else {
+			offset = new Vector2 (Time.time * scrollSpeed, 0f);
+		}
+		rend.material.mainTextureOffset = offset;
+	}
+}
---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	public void StartButton(){
+		SceneManager.LoadScene ("main");
+	}
+
+	public void ExitButton(){
+		Application.Quit ();
+	}
+}
